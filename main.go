0ba730eb651e28f98412be7b72a2363cfe36a3b6
Denton24646 <Denton24646@gmail.com>
2018-09-03 21:43:38 -0400

    add base 0x contracts
    add base 0x contracts

--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"math"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
 
 func main() {
-	fmt.Println("Hello matching engine")
+	//Connect to mainnet
+	client, err := ethclient.Dial("https://mainnet.infura.io")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Connect to ganache-cli
+	// client, err := ethclient.Dial("http://localhost:8545")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	fmt.Println("we have a connection")
+	_ = client // we'll use this in the upcoming sections
+
+	// address := common.HexToAddress("0x1903c48d5fb80933d0aeaf06775594b6124dc782")
+	// fmt.Println(address.Hex())
+	// fmt.Println(address.Hash().Hex())
+	// fmt.Println(address.Bytes())
+
+	account := common.HexToAddress("0x71c7656ec7ab88b098defb751b7401b5f6d8976f")
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balance) // 25893180161173005034
+
+	blockNumber := big.NewInt(5532993)
+	balanceAt, err := client.BalanceAt(context.Background(), account, blockNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balanceAt) // 25729324269165216042
+
+	fbalance := new(big.Float)
+	fbalance.SetString(balanceAt.String())
+	ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
+	fmt.Println(ethValue) // 25.729324269165216041
+
+	pendingBalance, err := client.PendingBalanceAt(context.Background(), account)
+	fmt.Println(pendingBalance) // 25729324269165216042
 }
